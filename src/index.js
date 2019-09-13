b921574e04fee6928d1aabaa4ec80905bc6e18a4
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-09-13 20:45:05 +0200

    Use legacy create-react-class
    Refactor to use legacy create-react-class

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import createReactClass from 'create-react-class';
 
-class Switch extends React.Component {
-  state = {
-    on: false,
-  };
+// class Switch extends React.Component {
+//   state = {
+//     on: false,
+//   };
 
-  handleToggle = () => this.setState(prevState => ({ on: !prevState.on }));
+//   handleToggle = () => this.setState(prevState => ({ on: !prevState.on }));
 
-  render() {
+//   render() {
+//     return (
+//       <div>
+//         {this.state.on ? 'On' : 'Off'}
+//         <div>
+//           <button onClick={this.handleToggle}>Toggle</button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+const Switch = createReactClass({
+  getInitialState: function() {
+    return {
+      on: false,
+    };
+  },
+  handleToggle: function() {
+    this.setState(prevState => ({ on: !prevState.on }));
+  },
+  render: function() {
     return (
       <div>
         {this.state.on ? 'On' : 'Off'}
@@ -17,8 +38,8 @@ class Switch extends React.Component {
         </div>
       </div>
     );
-  }
-}
+  },
+});
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<Switch />, rootElement);
