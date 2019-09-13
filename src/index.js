cb0da4a51b430762ae9b2d517c0cde4bcdfac782
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-09-13 20:47:04 +0200

    Go back to class component

--- src/index.js
+++ src/index.js
@@ -1,39 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import createReactClass from 'create-react-class';
 
-// class Switch extends React.Component {
-//   state = {
-//     on: false,
-//   };
+class Switch extends React.Component {
+  state = {
+    on: false,
+  };
 
-//   handleToggle = () => this.setState(prevState => ({ on: !prevState.on }));
+  handleToggle = () => this.setState(prevState => ({ on: !prevState.on }));
 
-//   render() {
-//     return (
-//       <div>
-//         {this.state.on ? 'On' : 'Off'}
-//         <div>
-//           <button onClick={this.handleToggle}>Toggle</button>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-const ToggleMixin = {
-  getInitialState: function() {
-    return {
-      on: false,
-    };
-  },
-  handleToggle: function() {
-    this.setState(prevState => ({ on: !prevState.on }));
-  },
-};
-
-const Switch = createReactClass({
-  mixins: [ToggleMixin],
-  render: function() {
+  render() {
     return (
       <div>
         {this.state.on ? 'On' : 'Off'}
@@ -42,8 +17,8 @@ const Switch = createReactClass({
         </div>
       </div>
     );
-  },
-});
+  }
+}
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<Switch />, rootElement);
